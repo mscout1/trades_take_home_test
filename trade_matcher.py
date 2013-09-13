2a627d4cc0b18d70afcc813fa27d8b77031dd1fc
mscout1 <matthew@PENELOPE>
2013-09-13 14:47:59 -0500

    add mappings for sdt values

--- trade_matcher.py
+++ trade_matcher.py
@@ -13,6 +13,46 @@ extention. See "MO Developer Take-Home Test.docx" for details
 
 import sys
 
+#Mappings for sdt values
+symbol_map = {
+    'JF'  : '35',
+    'QAL' : 'F-',
+    'RT'  : '1W',
+    }
+
+buysell_map= {
+    'B':'1',
+    'S':'2',
+    }
+
+otype_month_code = {
+    'A': ('1' , 'C'),
+    'B': ('2' , 'C'),
+    'C': ('3' , 'C'),
+    'D': ('4' , 'C'),
+    'E': ('5' , 'C'),
+    'F': ('6' , 'C'),
+    'G': ('7' , 'C'),
+    'H': ('8' , 'C'),
+    'I': ('9' , 'C'),
+    'J': ('10', 'C'),
+    'K': ('11', 'C'),
+    'L': ('12', 'C'),
+    'M': ('1' , 'P'),
+    'N': ('2' , 'P'),
+    'O': ('3' , 'P'),
+    'P': ('4' , 'P'),
+    'Q': ('5' , 'P'),
+    'R': ('6' , 'P'),
+    'S': ('7' , 'P'),
+    'T': ('8' , 'P'),
+    'U': ('9' , 'P'),
+    'V': ('10', 'P'),
+    'W': ('11', 'P'),
+    'X': ('12', 'P'),
+}
+
+
 class Trade(object):
     """Trade class uses the csv format as it's internal format"""
     def __init__(self, arg):
