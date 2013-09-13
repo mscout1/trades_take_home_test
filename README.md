8158f6bb3ed3578ff47f62645ebe0ca3dc56faed
mscout1 <matthew@PENELOPE>
2013-09-13 14:13:18 -0500

    sketch out design skeleton Start listing assumptions in README Ignore msword's silly hidden ~ files

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ trades_take_home_test
 =====================
 
 This is a take home test given to me by an employer
+
+
+Assumptions
+===========
+* Files will always have a single consistent internal format. (No mixing CSV and SDT in the same file)
+
+* There will always be a first line containing header information
+
+* The format of a file can be correctly determined by the presence of "," or ";" in the first line
+
+* The other contents of the first line are for human use only, not relevent to parsing of the files
+
+* Blank lines can be safely ignored.
+
+* Unparsable lines should be reported then ignored.
--- trade_matcher.py
+++ trade_matcher.py
@@ -1,2 +1,49 @@
+"""
+Trade Reconciler.
+
+This script reads in two files, each containing a list of trades'
+It then attempts to reconcile the two, printing a list of matches,
+and two lists of non-matches.
+
+This script can comprehend 2 different formats, csv and sdt
+(Comma Seperated Values and Semicolen Delimited Text)
+These formats vary in more then just seperator and file
+extention. See "MO Developer Take-Home Test.docx" for details
+"""
+
 import sys
-print(sys.version)
+
+class Trade(object):
+    """Trade class uses the csv format as it's internal format"""
+    def __init__(self, arg):
+        super(Trade, self).__init__()
+        self.arg = arg
+
+    @classmethod
+    def fromCSV(cls):
+        pass
+
+    @classmethod
+    def fromSDT(self):
+        pass
+
+    def __eq__(self):
+        pass
+
+    def __hash__():
+        pass
+
+    def __str__(self):
+        pass
+
+
+def main(filename1, filename2):
+    pass
+
+def reconcile(list1, list2):
+    """
+    returns (matches, only_in_1, only_in_2)
+    """
+
+if __name__ == '__main__':
+    main(*sys.argv[1:]) #TODO:replace this with argparse
